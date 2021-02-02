f6c03a9101609dd928486814442d23650274533d
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-02-02 18:15:34 +0100

    Update
    Réseau de neurones + Pre Process

--- 00_Preprocess_Music.py
+++ 00_Preprocess_Music.py
@@ -41,14 +41,28 @@ paths_df = pd.DataFrame({'genre': genre_Y, 'file_path': file_X})
 amplitudes_allsongs = {} 
 for i in range(len(paths_df)):
     print('track {}/{}'.format(i, len(paths_df)))
+    paths_df.loc[i, 'song_ID'] = paths_df.loc[i, 'file_path'].split('/')[-1]
     path_temp = paths_df.loc[i,'file_path']
     amplitude_temp, samplingrate = librosa.load(path_temp)
-    amplitudes_allsongs[path_temp.split('/')[-1]] = amplitude_temp
+    amplitudes_allsongs[paths_df.loc[i, 'song_ID']] = amplitude_temp
 
-#%%
+paths_df = paths_df.set_index('song_ID')
 
-for k in amplitudes_allsongs.keys():
-    
+
+#%% Random Choice of 10 musics
+import random as rnd
+sample10 = rnd.sample(list(paths_df.index), k=10)
+sample_genres = pd.DataFrame(paths_df.loc[sample10, 'genre']).reset_index()
+
+
+#%% Compute MFCCs
+sample_mfccs = {}
+for sID in sample_genres['song_ID']:
+     sample_mfccs[sID] = librosa.feature.mfcc(y=amplitudes_allsongs[sID], sr=samplingrate,
+                                              n_mfcc=30)
+#%% Plot 
+ax = sns.heatmap(sample_mfccs[sID])
+ax.invert_yaxis()
 
 
 
---
+++ 01_NN_Script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 17:33:24 2021
+
+@author: erwanrahis
+"""
+#Script to run the model for audio processing classification
+
+
+import tensorflow as tf
+print('Using TensorFlow version', tf.__version__)
+from tensorflow.keras.utils import to_categorical
+from matplotlib import pyplot as plt
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#%%
+#Sequential class of NN : pass a list with layers
+model = Sequential( [ #No need for input layer 
+    Dense(80, activation='relu', input_shape=(784,)), #Hidden dense layer (fully connected with ReLu activation)
+    Dense(128, activation='relu'), #Input shape implied automatically
+    Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='sgd',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']    
+)
+
+model.summary()
+
+#%% Fit the neural network
+model.fit(x_train_norm, y_train_encoded, epochs=3)
+
+#%%  
+loss, accuracy = model.evaluate(x_test_norm, y_test_encoded)
+print('Test set accuracy = ', accuracy*100)
