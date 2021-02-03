b336041215375524ce3c851b0d9a136e86d93f20
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-02-03 15:02:29 +0100

    YoutubeDDL

--- 03_DDL_Song.py
+++ 03_DDL_Song.py
@@ -5,28 +5,30 @@
 Script to download a song from youtube
 
 """
+#!youtube-dl --extract-audio --postprocessor-args "-ss 0:0:30 -to 0:1:00" --audio-format wav -o "%(title)s.%(ext)s.wav" "https://www.youtube.com/watch?v=l7MaKmKJqoc"
+
+
 import youtube_dl
 
 #%%
 #Youtube dl object
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+ydl = youtube_dl.YoutubeDL({'outtmpl': '%(title)s.%(ext)s', 
+                            'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                                'preferredcodec': 'wav'
+                                                }]})
 
-
+URL = 'https://www.youtube.com/watch?v=l7MaKmKJqoc'
 #Get the information
 with ydl:
     result = ydl.extract_info(
-        'https://www.youtube.com/watch?v=l7MaKmKJqoc',
+        URL,
         download=False )
 #print the title of the song
 print(result['title'])
 
 
-#%% 
-if 'entries' in result:
-    video = result['entries'][0]
-else:
-    video = result
+#%% Download
+ydl.download([URL])
 
-print(video)
-video_url = video['webpage_url']
-print(video_url)
+import sys
+!{sys.executable} -m youtube-dl --extract-audio --postprocessor-args "-ss 0:0:30 -to 0:1:00" --audio-format wav -o "%(title)s.%(ext)s.wav" "https://www.youtube.com/watch?v=l7MaKmKJqoc"
