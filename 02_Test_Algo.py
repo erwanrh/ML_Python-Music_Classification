def73723212fdccccdfc48d80c4e6d2b29191541
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-02-03 12:34:34 +0100

    Update

---
+++ 02_Test_Algo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+Use of the algorithm with a new data entry
+
+"""
+
+#%% Download a song
+!youtube-dl --extract-audio --postprocessor-args "-ss 0:0:30 -to 0:1:00" --audio-format mp3 -o "%(title)s.%(ext)s" "https://www.youtube.com/watch?v=l7MaKmKJqoc"
+
+#%%
+
+test_path = '/Users/erwanrahis/Documents/Cours/MS/S1/Machine_Learning_Python/ML_Python-Music_Classification.nosync/classic.wav'
+
+#%% Loading the new song
+amplitude_temp, samplingrate = librosa.load(test_path)
+# Loading the MFCCs
+temp_mfccs = librosa.feature.mfcc(y=amplitude_temp, 
+                                  sr=samplingrate,
+                                  n_mfcc=n_mfcc)
+
+#Compute mean mfccs
+mean_mfccstemp = []
+for i in range(n_mfcc):
+    mean_mfccstemp.append(np.mean(temp_mfccs[i,:]))
+
+
+
+#%% Test
+test_ = pd.DataFrame(mean_mfccstemp).transpose()
+pred = model.predict(test_)
+
+
+
+#%% Plot prediction probabilities
+plt.figure(figsize=(10,5))
+plt.bar(x=classes,height=pred[0])
+plt.title('Classification probabilities')
+
+
