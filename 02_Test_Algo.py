1b53767b1bc9ac66b2e72c6ec3e1435672a4bee3
lbenbaccar <lilia.benbaccar@gmail.com>
2021-02-03 14:42:17 +0100

    Update 02_Test_Algo.py

--- 02_Test_Algo.py
+++ 02_Test_Algo.py
@@ -6,8 +6,10 @@
 Use of the algorithm with a new data entry
 
 """
-
-
+import librosa
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 #%%
 
 test_path = '/Users/erwanrahis/Documents/Cours/MS/S1/Machine_Learning_Python/ML_Python-Music_Classification.nosync/classic.wav'
@@ -15,6 +17,7 @@ test_path = '/Users/erwanrahis/Documents/Cours/MS/S1/Machine_Learning_Python/ML_
 #%% Loading the new song
 amplitude_temp, samplingrate = librosa.load(test_path)
 # Loading the MFCCs
+n_mfcc = 30
 temp_mfccs = librosa.feature.mfcc(y=amplitude_temp, 
                                   sr=samplingrate,
                                   n_mfcc=n_mfcc)
