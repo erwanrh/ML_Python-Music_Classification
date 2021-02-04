610b71e6c52c6050a68a371b3b3f7ecb977a79a8
erwanrh <48155512+erwanrh@users.noreply.github.com>
2021-02-04 10:41:59 +0100

    NN

--- 01_NN_Script.py
+++ 01_NN_Script.py
@@ -11,48 +11,29 @@ This script contains
     * DataViz import to plot the metrics
     
 """
-
-
-
+# Libraries
 import tensorflow as tf
-print('Using TensorFlow version', tf.__version__)
 from tensorflow.keras.utils import to_categorical
 from matplotlib import pyplot as plt
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split`
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.metrics import Recall, Precision, Accuracy
 import FunctionsDataViz
+import FunctionsNN
 
 #%% Table with the results
-all_results = pd.DataFrame(columns=['Model', 'Accuracy', 'Precision', 'Recall'])
-
-
-#%% Modèle 1 : Modèle de NN avec 30  Moyennes des MFCCS en input 
-
-model1 = Sequential( [ 
-    Dense(30, activation='relu', input_shape=(30,)), #Hidden dense layer (fully connected with ReLu activation)
-    Dense(20, activation='relu'), #Input shape implied automatically
-    Dense(15, activation='linear'),
-    Dense(10, activation='linear'),
-    Dense(10, activation='softmax')
-])
-
-model1.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=[Accuracy(), Precision(), Recall()]    
-)
-
-
-model1.summary()
+all_results = pd.DataFrame(columns=['Model','Optimizer','Epochs', 'Batch',
+                                    'Test_Accuracy', 'Test_Precision', 'Test_Recall'])
 
 #%%
 """
 Prepare the data
 """
+#Features
+X = mean_mfccs
 
 #One hot encoding on the labels 
 encoder = LabelEncoder()
@@ -62,43 +43,35 @@ encoded_Y = encoder.transform(paths_df['genre'])
 #Get the classes of the encoder
 classes= encoder.classes_.tolist()
 
-#Features and labels
-y = to_categorical(encoded_Y)
-X = mean_mfccs
-
-#Train/Test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-
-#%%
+#Hyperparameters
+n_epochsList = [800]
+n_batchList = [None]  
+    
+#%% 
 """
-Hyperparameters optimization
+Modèle 1 : Modèle de NN avec 30  Moyennes des MFCCS en input 
 """
-#Hyperparameter 
-n_epochsList = [100, 150, 70]
-n_batchList = [None, 10, 50, 200]
 
-i=1
-for n_epochs in n_epochsList:
-    print('Epoch {}/{}'.format(i, len(n_epochsList)))
-    j=1
-    for n_batch in n_batchList:
-        print('Batch {}/{}'.format(j, len(n_batchList)))
-        # Fit model on train sample
-        model1.fit(X_train, y_train, epochs=n_epochs, batch_size=n_batch, verbose=0)
-        # Evaluate on test sample     
-        loss, accuracy, precision, recall = model1.evaluate(X_test, y_test)
-        all_results = all_results.append({'Model':'NN_30meanMFCCs',
-                                          'Epochs': n_epochs,
-                                          'Batch': n_batch, 
-                                          'Accuracy':accuracy*100,
-                                          'Precision':precision*100,
-                                          'Recall':recall*100}, ignore_index=True)
-        j+=1
-    i+=1
-#%%Results
+model_name= 'NN_30meanMFCCs'
+optimizer_ = 'adam'
+model_object = Sequential( [ 
+        Dense(30, activation='relu', input_shape=(30,)), #Hidden dense layer (fully connected with ReLu activation)
+        Dense(25, activation='linear'),
+        Dense(20, activation='relu'),
+        Dense(15, activation='linear'),
+        Dense(10, activation='softmax')
+        
+    ])
+model_object.compile(
+    optimizer=optimizer_,
+    loss='categorical_crossentropy',
+    metrics=[Accuracy(), Precision(), Recall()]    
+)
+model_object.summary()
 
-print(all_results)
-all_results.loc[all_results['Batch'].isna(),'Batch']='NoBatch'
-#%%
-fig = FunctionsDataViz.plot_metricsNN(all_results)
-fig.savefig('Data_viZModel1.png', dpi=500)
+
+all_results = all_results.append(FunctionsNN.fit_NeuralNetwork(X, encoded_Y,model_name,
+                                                               model_object, n_epochsList,
+                                                               n_batchList, optimizer_,
+                                                               save_plot=False)) 
+
